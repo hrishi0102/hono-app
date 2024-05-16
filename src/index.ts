59cfaa2ccf6ee78db6dd57f101c72116b7895f98
Hrishikesh Patil <hrishi0102business@gmail.com>
2024-05-16 20:20:23 +0530

    db connection pooling w prisma

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
-import { Hono } from "hono";
+import { Hono, Next } from "hono";
+import { PrismaClient } from "@prisma/client/edge";
+import { withAccelerate } from "@prisma/extension-accelerate";
+import { env } from "hono/adapter";
 
 const app = new Hono();
 
@@ -10,15 +13,48 @@ async function MiddleAuth(c: any, next: any) {
     return c.text("You dont have acces");
   }
 }
-app.use(MiddleAuth);
+// app.use(MiddleAuth);
 
-app.post("/", async (c) => {
-  const body = await c.req.parseBody();
-  console.log(body);
-  console.log(c.req.header("Authorization"));
-  console.log(c.req.query("param"));
+// app.post("/", async (c) => {
+//   const body = await c.req.parseBody();
+//   console.log(body);
+//   console.log(c.req.header("Authorization"));
+//   console.log(c.req.query("param"));
 
+//   return c.text("Hello Hono!");
+// });
+
+app.get("/", async (c) => {
   return c.text("Hello Hono!");
+});
+
+//Database Connection
+app.post("/", async (c) => {
+  // Todo add zod validation here
+  const body: {
+    name: string;
+    email: string;
+    password: string;
+  } = await c.req.json();
+  const { DATABASE_URL } = env<{ DATABASE_URL: string }>(c);
+
+  const prisma = new PrismaClient({
+    datasourceUrl: DATABASE_URL,
+  }).$extends(withAccelerate());
+
+  console.log(body);
+
+  const response = await prisma.user.create({
+    data: {
+      name: body.name,
+      email: body.email,
+      password: body.password,
+    },
+  });
+
+  console.log(JSON.stringify(response));
+
+  return c.json({ msg: "as" });
 });
 
 export default app;
